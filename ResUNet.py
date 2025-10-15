97de82aa40e9c6cfea8e35602963158a7739a849
Yehong Huang <hyh159263@gmail.com>
2025-10-15 19:49:18 -0400

    Got UNet and AttnUNet working

---
+++ ResUNet.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class ResUNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResUNet, self).__init__()
+    
+    def forward(self, x):
+
+        
+        return x
+    
